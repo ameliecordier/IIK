9b36c98b56aedfdf7f9f649aecfb5635146ef9ee
Amélie Cordier <acordier@gmail.com>
2016-02-06 19:11:43 +0100

    Création du module analyser (refactoring needed)

---
+++ datahandler/analyser.py
@@ -0,0 +1,39 @@
+#-*- coding: utf-8 -*-
+from datahandler import expertPatterns
+from datahandler import miningPatterns
+import operator
+
+
+def sortByFreq(mp):
+    """
+    Trie un résultat de type miningPatterns par fréquence
+    Retourne un objet de type miningPatterns
+    :param mp: un objet de type miningPatterns
+    :return:
+    """
+
+    mpFreq = miningPatterns.MiningPatterns()
+    mpFreq.results = sorted(mp.results, key=lambda x: int(operator.itemgetter(4)(x)), reverse=True)
+    return mpFreq
+
+
+
+def findPatterns(expertPatterns, miningPatterns):
+    """
+    Recherche dans les patterns minés les patterns identifiés par l'expert
+    :param expertPatterns: la structure des patterns de l'expert
+    :param miningPatterns: les patterns issus de la fouille
+    :return: TODO
+    """
+    for line in miningPatterns.results:
+        pattern = line[1]
+        try:
+            idx = expertPatterns.patterns.index(pattern)
+            print("Pattern fouillé : "+ pattern)
+            print("Pattern expert : "+expertPatterns.patterns[idx])
+        except ValueError:
+            print("Pattern non trouvé : "+pattern)
+            pass
+
+
+
