0ae35ecd53aa1dd8521b235d09de264801993a05
Amélie Cordier <acordier@gmail.com>
2016-02-07 11:14:20 +0100

    Refactoring de sortBy pour ne faire qu'une méthode

--- datahandler/analyser.py
+++ datahandler/analyser.py
@@ -4,29 +4,20 @@ from datahandler import miningPatterns
 import operator
 
 
-def sortByFreq(mp):
+def sortBy(col, mp):
     """
-    Trie un résultat de type miningPatterns par fréquence
-    Retourne un objet de type miningPatterns
+    Trie un résultat de type miningPatterns par la colonne choisie
+    :param col: la colonne selon laquelle trier (int)
+        possibilités pour col :
+            0 : rang brut
+            4 : fréquence
+            7 : couverture
     :param mp: un objet de type miningPatterns
-    :return:
+    :return: un objet de type miningPatterns
     """
     mpFreq = miningPatterns.MiningPatterns()
-    mpFreq.results = sorted(mp.results, key=lambda x: int(operator.itemgetter(4)(x)), reverse=True)
+    mpFreq.results = sorted(mp.results, key=lambda x: int(operator.itemgetter(col)(x)), reverse=True)
     return mpFreq
-
-def sortByCouv(mp):
-    """
-    Trie un résultat de type miningPatterns par couverture
-    Retourne un objet de type miningPatterns
-    :param mp: un objet de type miningPatterns
-    :return:
-    """
-    mpFreq = miningPatterns.MiningPatterns()
-    mpFreq.results = sorted(mp.results, key=lambda x: int(operator.itemgetter(7)(x)), reverse=True)
-    return mpFreq
-
-
 
 
 def findPatterns(expertPatterns, miningPatterns):
